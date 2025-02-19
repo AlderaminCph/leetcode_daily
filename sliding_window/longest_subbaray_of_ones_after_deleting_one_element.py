3ba897866c9ecd9316d5fc4ea1931edae7203600
AlderaminCph <sypkova.anastasia@yandex.ru>
2025-02-19 11:21:24 +0300

    enable one more test

--- sliding_window/longest_subbaray_of_ones_after_deleting_one_element.py
+++ sliding_window/longest_subbaray_of_ones_after_deleting_one_element.py
@@ -16,8 +16,9 @@ Example 1:
 Explanation: After deleting the number in position 2, [1,1,1] contains 3 numbers with value of 1's.
 Example 2:
 
-Input: nums = [0,1,1,1,0,1,1,0,1]
-Output: 5
+>>> Solution().longestSubarray(nums = [0,1,1,1,0,1,1,0,1])
+5
+
 Explanation: After deleting the number in position 4, [0,1,1,1,1,1,0,1]
 longest subarray with value of 1's is [1,1,1,1,1].
 Example 3:
