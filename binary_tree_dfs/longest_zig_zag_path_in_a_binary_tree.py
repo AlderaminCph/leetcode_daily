02e508d673fb5d2219214f5252e9920331c3c75c
AlderaminCph <sypkova.anastasia@yandex.ru>
2025-03-16 14:19:15 +0300

    first attemt to solve 1372. Longest ZigZag Path in a Binary Tree

---
+++ binary_tree_dfs/longest_zig_zag_path_in_a_binary_tree.py
@@ -0,0 +1,69 @@
+"""
+1372. Longest ZigZag Path in a Binary Tree
+
+You are given the root of a binary tree.
+
+A ZigZag path for a binary tree is defined as follow:
+
+    Choose any node in the binary tree and a direction (right or left).
+    If the current direction is right, move to the right child of the current node;
+    otherwise, move to the left child.
+    Change the direction from right to left or from left to right.
+    Repeat the second and third steps until you can't move in the tree.
+
+Zigzag length is defined as the number of nodes visited - 1.
+(A single node has a length of 0).
+
+Return the longest ZigZag path contained in that tree.
+
+Example 1:
+
+Input: root = [1,null,1,1,1,null,null,1,1,null,1,null,null,null,1]
+Output: 3
+Explanation: Longest ZigZag path in blue nodes (right -> left -> right).
+
+Example 2:
+
+Input: root = [1,1,1,null,1,null,null,1,1,null,1]
+Output: 4
+Explanation: Longest ZigZag path in blue nodes (left -> right -> left -> right).
+
+Example 3:
+
+Input: root = [1]
+Output: 0
+
+"""
+
+from typing import Literal
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        """
+        Definition for a binary tree node.
+        """
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def longestZigZag(self, root: TreeNode | None) -> int:
+        if not root or not (root.left and root.right):
+            return 0
+
+        return max(self.maxZigZag(root, "left"), self.maxZigZag(root, "right")) + 1
+
+    def maxZigZag(self, node: TreeNode, direction: Literal["right", "left"]) -> int:
+        if not node:
+            return 0
+
+        if direction == "right":
+            node = node.right
+            direction = "left"
+        else:
+            node = node.left
+            direction = "right"
+
+        return self.maxZigZag(node, direction)
